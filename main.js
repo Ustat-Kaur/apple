a18ba96e37600d4d69256fe67528afe2f1cb9e0c
Ustat-Kaur <81715255+Ustat-Kaur@users.noreply.github.com>
2022-05-28 14:26:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+screen_width = 0;
+screen_height = 0;
+
+apple = "";
+speak_data = "";
+to_number = "";
+
+function preload(){
+  apple = loadImage("apple.png");
+}
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ to_number = Number(content);
+
+ if(Number.isInteger(to_number)){
+   draw_apple = "set";
+   document.getElementById("status").innerHTML = to_number + "Started Drawing Apple";
+ }
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ screen_width = window.innerWidth;
+ screen_height = window.innerHeight;
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
