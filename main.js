f9bb0f4c488818d09013295d62c7a8e06f9b46cc
Ustat-Kaur <81715255+Ustat-Kaur@users.noreply.github.com>
2022-06-09 20:09:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -28,14 +28,14 @@ recognition.onresult = function(event) {
 
  console.log(event); 
 
+ content = event.results[0][0].transcript;
+
  to_number = Number(content);
 
  if(Number.isInteger(to_number)){
    draw_apple = "set";
    document.getElementById("status").innerHTML = to_number + "Started Drawing Apple";
  }
-
- content = event.results[0][0].transcript;
 
     document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
 
@@ -44,13 +44,23 @@ recognition.onresult = function(event) {
 function setup() {
  screen_width = window.innerWidth;
  screen_height = window.innerHeight;
+
+ canvas = createCanvas(screen_width, screen_height-150);
+ canvas.position(0,150);
 }
 
 function draw() {
   if(draw_apple == "set")
   {
+    for (var i = 1; i <= to_number; i++)
+    {
+      x = Math.floor(Math.random() * 700);
+      y = Math.floor(Math.random() * 400);
+      image(apple, x, y, 50, 50);
+    }
     document.getElementById("status").innerHTML = to_number + " Apples drawn";
     draw_apple = "";
+    speak();
   }
 }
 
